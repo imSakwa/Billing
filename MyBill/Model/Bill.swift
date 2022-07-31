c61e074afb84eca1060738ccef3f9dbd7f309d5a
ChangMin <sopg04@gmail.com>
2022-07-31 23:18:57 +0900

    feat: add Bill Model

---
+++ MyBill/Model/Bill.swift
@@ -0,0 +1,15 @@
+//
+//  Bill.swift
+//  MyBill
+//
+//  Created by ChangMin on 2022/07/31.
+//
+
+import Foundation
+
+struct Bill {
+    let title: String
+    let cost: Int
+    let memo: String
+    let date: String
+}
