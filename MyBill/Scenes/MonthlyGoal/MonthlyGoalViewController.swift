d37361036af3b8863165a2fc3172b15bc6012a86
ChangMin <sopg04@gmail.com>
2022-08-16 23:07:57 +0900

    style: Monthly Goal VC 레이아웃 작업중

--- MyBill/Scenes/MonthlyGoal/MonthlyGoalViewController.swift
+++ MyBill/Scenes/MonthlyGoal/MonthlyGoalViewController.swift
@@ -11,15 +11,32 @@ import UIKit
 final class MonthlyGoalViewController: UIViewController {
     private lazy var presenter = MonthlyGoalPresenter(viewController: self)
     
+    private lazy var settingButton: UIBarButtonItem = {
+        let image = UIImage(named: "gearshape")
+        let barButton = UIBarButtonItem(image: image, style: .plain, target: self, action: nil)
+        return barButton
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
 }
 
 extension MonthlyGoalViewController: MonthlyGoalProtocol {
     func setupNavigationBar() {
         view.backgroundColor = .systemBackground
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        self.navigationItem.rightBarButtonItem = settingButton
+        
     }
     
     func setupLayout() {
